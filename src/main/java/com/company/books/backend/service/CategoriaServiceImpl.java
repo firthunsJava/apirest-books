a9c8229b9bebe5963f270a333e63186b6219f7bd
firthuns <firthunsJava@gmail.com>
2022-10-02 13:24:43 +0200

    get cagetgoria completo y funcionando

---
+++ src/main/java/com/company/books/backend/service/CategoriaServiceImpl.java
@@ -0,0 +1,54 @@
+package com.company.books.backend.service;
+
+import com.company.books.backend.model.Categoria;
+import com.company.books.backend.model.dao.ICategoriaDao;
+import com.company.books.backend.response.CategoriaResponseRest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class CategoriaServiceImpl implements ICategoriaService{
+
+    private static final Logger log = LoggerFactory.getLogger(CategoriaServiceImpl.class);
+
+    @Autowired
+    private ICategoriaDao categoriaDao;
+    // 1
+    @Override
+    @Transactional(readOnly = true) //
+    public ResponseEntity<CategoriaResponseRest> buscarCategoria() {
+
+        log.info("inicio método buscarCategorias()");
+
+        CategoriaResponseRest response = new CategoriaResponseRest();
+
+        try {
+
+            List<Categoria> categoria = (List<Categoria>) categoriaDao.findAll();
+
+            response.getCategoriaResponse().setCategoria(categoria);
+
+
+            response.setMetada("Respuesta ok", "00", "Respuesta exitosa");
+
+        }catch(Exception e) {
+            response.setMetada("Respuesta nok", "-1","Respuesta incorrecta");
+            log.error("Error al consultar categorias: ", e.getMessage());
+            e.getStackTrace();
+            return new ResponseEntity<CategoriaResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new  ResponseEntity<CategoriaResponseRest> (response, HttpStatus.OK); //devuelve 200
+    }
+
+}
+
+
+
+
